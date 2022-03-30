eb0fb6f78dcf8b6f2c4f124a639672c760ed3a92
SimoneScapin <simonescap87@gmail.com>
2022-03-30 14:32:56 +0200

    first commit with bootstrap and axios installed

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headerContent',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
